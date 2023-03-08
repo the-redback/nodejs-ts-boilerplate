e60a61e0b7f6b6d61033f379b3baa335f8238421
the-redback <maruf.2hin@gmail.com>
2023-03-08 17:14:21 -0600

    xo added for eslinter

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import http from "http";
+import http from 'node:http';
 
-export const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "It Works!!!",
-    })
-  );
+export const server = http.createServer((request, res) => {
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	res.end(
+		JSON.stringify({
+			data: 'It Works!!!',
+		}),
+	);
 });
 
 server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000/");
+	console.log('Server running on http://localhost:3000/');
 });
